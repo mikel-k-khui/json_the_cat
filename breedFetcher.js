43b43ab93b7831adb25efe51265856be2d58e8c4
Mike Chui <mikel.k.khui@gmail.com>
2019-08-01 09:05:54 -0600

    TCP GET practice

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,45 +1,31 @@
 let query = {
 method: 'GET',
 url: 'https://api.thecatapi.com/v1/breeds/search',
-qs: { name: '' },
+qs: { name: 'howdy' },
 headers: { 'x-api-key': 'DEMO-API-KEY' }
 };
 
 const request = require('request');
 let data;
 
-query.qs.name = process.argv[2];
+if (process.argv[2] === "") {
+  return "Please enter a breed of cat!";
+}
 
-console.log(query);
+query.qs.name = process.argv[2];
+// console.log(query.qs.name);
 
 request(query, function (error, response, body) {
-  if (error) throw new Error(error);
-  console.log(body);
-  console.log(typeof body)
+  if (error) throw new Error('Failed to request details: ', error);
+  // console.log(body);
+  // console.log(typeof body)
 
   data = JSON.parse(body);
-  console.log(data);
-  console.log(typeof data);
-});
 
-const breedName = process.argv[2];
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
-
-request(url, (error, resp, body) => {
-  // console.log(body);
-  // console.log(typeof body);
-
-  if (error) {
-    return console.log('Failed to request details: ', error);
-  }
-
-  const data = JSON.parse(body);
   // console.log(data);
+  // console.log(data[0].description);
+  // console.log(typeof data);
+  
+  (data ? console.log(data[0].description) : console.log(`Failed to find breed ${query.qs.name}`));
 
-  const breed = data[0];
-  if (breed) {
-    console.log(breed.description);
-  } else {
-    console.log(`Failed to find breed ${breedName}`);
-  }
 });
